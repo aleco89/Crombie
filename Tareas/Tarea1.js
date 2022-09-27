58448d9fa9729689ee06734040552b9a0971e8b6
Ana <ana_leconte@hotmail.com>
2022-09-27 09:14:45 -0300

    Tarea 1

---
+++ Tareas/Tarea1.js
@@ -0,0 +1,32 @@
+
+const personA={
+    age:20,
+    hasDocument:false
+}
+const personB={
+    age:19,
+    hasDocument:false
+}
+const personC={
+    age:16,
+    hasDocument:false
+}
+const personD={
+    age:22,
+    hasDocument:false
+}
+
+function canPass(person){
+    if (person.age>=18 && person.age<=20 && person.hasDocument==true){
+       return "pudiste pasar con tu documento";  
+    }else if(person.age>20){
+        return "pudiste pasar";
+    }else{
+        return "no pudiste pasar";
+    }
+}
+
+console.log(canPass(personA));
+console.log(canPass(personB));
+console.log(canPass(personC));
+console.log(canPass(personD));
