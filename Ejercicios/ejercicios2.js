24ef83e97c3361b4a9414f6fb6b65c944551274c
Ana <ana_leconte@hotmail.com>
2022-09-29 14:08:55 -0300

    ejercicios clase dos terminados

--- Ejercicios/ejercicios2.js
+++ Ejercicios/ejercicios2.js
@@ -10,20 +10,50 @@ for (i = 0; i < notas.length; i++) {
 console.log("La nota menor es: " + notaMenor);
 
 //ejercicio 2
+
 let alumnos = [
   { alumno: "Ana", nota: 8 },
+  { alumno:"Martín", nota:10 },
+  { alumno:"Clara", nota:10 },
   { alumno: "Pedro", nota: 7 },
   { alumno: "Maria", nota: 3 },
 ];
-let alumnoMayorNota = [];
+
+let alumnoMayorNota = [{ alumno: "nombre", nota: 0 }];
 for (i = 0; i < alumnos.length; i++) {
-  if (alumnos[i].nota > alumnoMayorNota.nota) {
-    alumnoMayorNota[0] = alumnos[i];
-  } //else{
-  // if ();
-  //}
+  if (alumnos[i].nota > alumnoMayorNota[0].nota){   	
+    alumnoMayorNota[0]=alumnos[i];     
+  }else if ((alumnos[i].nota)== (alumnoMayorNota[0].nota)) {
+		alumnoMayorNota.push(alumnos[i]);  
+  }
 }
-console.log("El alumno con la mayor nota es: " + alumnoMayorNota);
+console.log("El o los alumnos con mejor nota son:")
+console.log(alumnoMayorNota);
+
 
 //ejercicio 3
-numeros = [5, 6, 22, 5, 13, 8, 3, 10, 2, 7];
+arrayNumeros = [5, 6, 22, 9, 13, 8, 3, 10, 2, 7];
+
+for (i=0;i<arrayNumeros.length;i++){
+  if(arrayNumeros[i]<arrayNumeros[i-1]){
+    aux=arrayNumeros[i];
+    arrayNumeros[i]=arrayNumeros[i-1];
+    arrayNumeros[i-1]=aux
+    i=0;
+  }
+}
+console.log("Secuencia ordenada:")
+console.log(arrayNumeros)
+
+//ejercicio 4
+//Opcion A:
+arrayCaracteres= ["C", "r", "o", "N", "o", "P", "i", "o", "S"]
+mayusculas=[]
+
+for (i=0;i<arrayCaracteres.length;i++){
+  if (arrayCaracteres[i]<"["){
+    mayusculas.push(arrayCaracteres[i]);
+  }
+}
+
+console.log(mayusculas)
