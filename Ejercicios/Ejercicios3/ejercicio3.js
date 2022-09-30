9091f6d51a9c32b446d8c7b1d419473b4381c066
Ana <ana_leconte@hotmail.com>
2022-09-30 10:15:42 -0300

    ejercicios clase con  herlpers

---
+++ Ejercicios/Ejercicios3/ejercicio3.js
@@ -0,0 +1,18 @@
+
+let funciones = require("./helpers/funciones")
+
+//Solución A
+str1="escritorio"
+str2="silla"
+console.log(funciones.longestString(str1, str2));
+
+//Solución B
+
+arr=["Esto", "es", "un", "hola", "mundo"];
+str="hola";
+console.log(funciones.exist({ a: arr, s: str }));
+
+//Solución C
+
+let p = [{ firstName: "Ana", lastName: "Leconte" }];
+console.log(funciones.sayFullName(p));
---
+++ Ejercicios/Ejercicios3/helpers/funciones.js
@@ -0,0 +1,25 @@
+//Recibir dos strings como parámetros y devolver el de más letras.
+function longestString(s1, s2) {
+  if (s1.length > s2.length) {
+    return "La palabra más larga es: " + s1;
+  } else {
+    return "La palabra más larga es: " + s2;
+  }
+}
+
+//Recibir un arreglo y un string, devolver el índice en que se encuentra la palabra. Si no la encuentra retornar -1
+function exist({ a, s }) {
+  if (a.includes(s)) {
+    return "La palabra se encuentra en el índice: " + a.indexOf(s);
+  } else {
+    return -1;
+  }
+}
+
+//Recibir como parámetro un objeto con los atributos firstName y lastName.Consologuear nombre completo.
+function sayFullName(obj) {
+  return "El nombre completo es: " + obj[0].firstName + " " + obj[0].lastName;
+}
+
+//EXPORT:
+module.exports = { longestString, exist, sayFullName };
